5189300fd7f822a5560c4ce627420842d94c16ff
chunghoangtu <chunghoangtu@gmail.com>
2026-09-03 16:33:57 +0700

    chore: update commentary for docker compose and commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,44 @@
 export default {
   extends: ["@commitlint/config-conventional"]
 };
+
+// export default {
+//   extends: ["@commitlint/config-conventional"],
+//   parserPreset: {
+//     parserOpts: {
+//       headerPattern: /^(\w+)-\[([A-Z]+-\d+)\]: (.+)$/,
+//       headerCorrespondence: ["type", "ticket", "subject"],
+//     },
+//   },
+// };
+
+// export default {
+//   extends: ["@commitlint/config-conventional"],
+//   parserPreset: {
+//     parserOpts: {
+//       headerPattern: /^(\w+)-\[([A-Z][A-Z0-9]+-\d+)\]: (.+)$/,
+//       headerCorrespondence: ["type", "ticket", "subject"],
+//     },
+//   },
+//   rules: {
+//     "type-enum": [
+//       2,
+//       "always",
+//       [
+//         "feat",
+//         "fix",
+//         "chore",
+//         "docs",
+//         "style",
+//         "refactor",
+//         "perf",
+//         "test",
+//         "build",
+//         "ci",
+//         "revert",
+//       ],
+//     ],
+//     "type-case": [2, "always", "lower-case"],
+//     "subject-empty": [2, "never"],
+//   },
+// };
